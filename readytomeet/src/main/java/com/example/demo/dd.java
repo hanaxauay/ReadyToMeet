ef6d6086229771c85cbb8c761fb175028c33a752
김윤한 <88180966+YunHanKIM@users.noreply.github.com>
2023-05-31 16:22:06 +0900

    test111111

---
+++ readytomeet/src/main/java/com/example/demo/dd.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class dd {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ReadytomeetApplication.class, args);
+
+        System.out.println("zzz");
+    }
+}
