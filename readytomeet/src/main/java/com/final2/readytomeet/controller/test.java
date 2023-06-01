8aaae329c19a469e547123d8bccb520d4b9870e2
김진하 <128116382+KimJinHaa@users.noreply.github.com>
2023-06-01 15:29:07 +0900

    test11

---
+++ readytomeet/src/main/java/com/final2/readytomeet/controller/test.java
@@ -0,0 +1,17 @@
+package com.final2.readytomeet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class test {
+  @GetMapping("/all")
+  public String test1(){
+    return "all";
+  }
+  @GetMapping("/community")
+  public String test2() {
+      return "community";
+  }
+
+}
