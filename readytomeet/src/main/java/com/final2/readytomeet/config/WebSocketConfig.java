5430a776d641b4d8f0cb346eb08b48fbace6e99f
LeeHusung <mn040820@naver.com>
2023-06-02 16:32:56 +0900

    d

---
+++ readytomeet/src/main/java/com/final2/readytomeet/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.final2.readytomeet.config;
+
+import com.final2.readytomeet.chat.ChatHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new ChatHandler(), "/chat").setAllowedOrigins("*");
+    }
+}
--- readytomeet/src/main/java/com/final2/readytomeet/controller/ChatController.java
+++
@@ -1,7 +0,0 @@
-package com.final2.readytomeet.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class ChatController {
-}
