35edc1af08d86f0a0c9c3cf2180dd4ca2174968c
김윤한 <88180966+YunHanKIM@users.noreply.github.com>
2023-06-02 14:41:13 +0900

    이미지 중

---
+++ readytomeet/src/main/java/com/final2/readytomeet/model/UploadFile.java
@@ -0,0 +1,9 @@
+package com.final2.readytomeet.model;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public class UploadFile {
+    private List<MultipartFile> files;
+}
