54705d5d30899b8b1ec500b8bad5544435f591e3
김윤한 <88180966+YunHanKIM@users.noreply.github.com>
2023-06-03 17:15:24 +0900

    다 주석

--- readytomeet/src/main/java/com/final2/readytomeet/dto/ImageUploadDto.java
+++ readytomeet/src/main/java/com/final2/readytomeet/dto/ImageUploadDto.java
@@ -1,12 +1,12 @@
-package com.final2.readytomeet.dto;
-
-import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-
-@Data
-public class ImageUploadDto {
-    private MultipartFile file;
-
-}
+//package com.final2.readytomeet.dto;
+//
+//import lombok.Data;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.util.List;
+//
+//@Data
+//public class ImageUploadDto {
+//    private MultipartFile file;
+//
+//}
--- readytomeet/src/main/java/com/final2/readytomeet/service/ImageService.java
+++ readytomeet/src/main/java/com/final2/readytomeet/service/ImageService.java
@@ -1,35 +1,35 @@
-package com.final2.readytomeet.service;
-
-import com.final2.readytomeet.repository.ImageRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import org.springframework.stereotype.Service;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.UUID;
-
-@RequiredArgsConstructor
-@Service
-public class ImageService {
-
-    private final ImageRepository imageRepository;
-
-//    @Value("")
-//    private String uploadFolder;
-
-//    public void upload(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
-//        //동일 사진 업로드 했을 때 사진 덮어 씌워지는거 방지
-//        UUID uuid = UUID.randomUUID();
-//        String imageFileName = uuid + "_" + imageUploadDto.getFile().getOrignalFilename();
+//package com.final2.readytomeet.service;
 //
-//        Path imageFilePath = Paths.get(uploadFolder + imageFileName);
+//import com.final2.readytomeet.repository.ImageRepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.Value;
+//import org.springframework.stereotype.Service;
 //
-//        try {
-//            Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
-//        } catch(Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-}
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.util.UUID;
+//
+//@RequiredArgsConstructor
+//@Service
+//public class ImageService {
+//
+//    private final ImageRepository imageRepository;
+//
+////    @Value("")
+////    private String uploadFolder;
+//
+////    public void upload(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
+////        //동일 사진 업로드 했을 때 사진 덮어 씌워지는거 방지
+////        UUID uuid = UUID.randomUUID();
+////        String imageFileName = uuid + "_" + imageUploadDto.getFile().getOrignalFilename();
+////
+////        Path imageFilePath = Paths.get(uploadFolder + imageFileName);
+////
+////        try {
+////            Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+////        } catch(Exception e) {
+////            e.printStackTrace();
+////        }
+////    }
+//}
