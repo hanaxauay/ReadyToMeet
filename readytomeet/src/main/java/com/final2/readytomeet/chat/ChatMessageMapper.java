ff2646785fdff39328c07511378ce22362389de7
YeongjuDev <128116367+ChaYeongju@users.noreply.github.com>
2023-06-03 17:46:53 +0900

    채팅 관련 파일 주석 처리
    import 패키지 오류로 모두 주석 처리함

--- readytomeet/src/main/java/com/final2/readytomeet/chat/ChatMessageMapper.java
+++ readytomeet/src/main/java/com/final2/readytomeet/chat/ChatMessageMapper.java
@@ -1,17 +1,17 @@
-package com.final2.readytomeet.chat;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
-
-@Mapper
-public interface ChatMessageMapper {
-
-    ChatMessage getChatMessageById(@Param("id") Long id);
-
-    List<ChatMessage> getAllChatMessages();
-
-    void saveChatMessage(ChatMessage chatMessage);
-
-}
+//package com.final2.readytomeet.chat;
+//
+//import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Param;
+//
+//import java.util.List;
+//
+//@Mapper
+//public interface ChatMessageMapper {
+//
+//    ChatMessage getChatMessageById(@Param("id") Long id);
+//
+//    List<ChatMessage> getAllChatMessages();
+//
+//    void saveChatMessage(ChatMessage chatMessage);
+//
+//}
