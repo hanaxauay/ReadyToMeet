688c5e9fcf10440984ad65a2ee4eb667fe01cd8f
hana <jjss77777@naver.com>
2023-06-06 04:22:58 +0900

    이름 대충 짓지 말기로 해요 !!
    통일감있게 ~ 남들도 알아 볼 수 있게 ~

--- readytomeet/src/main/java/com/final2/readytomeet/controller/TestController.java
+++ readytomeet/src/main/java/com/final2/readytomeet/controller/TestController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class test {
+public class TestController {
   @GetMapping("/all")
   public String gotoAll(){
     return "all";
